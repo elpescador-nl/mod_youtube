6b2e09edf8238ea8f3b7a5fab35db882099d2619
Mark Visser <mark@elpescador.nl>
2017-04-04 23:29:57 +0200

    initial commit

---
+++ mod_youtube.php
@@ -0,0 +1,22 @@
+<?php
+
+defined('_JEXEC') or die;
+
+$videoid = $params->get('videoid');
+$playlist = $params->get('playlist');
+
+$label = preg_replace('/[^a-zA-Z0-9]/', '', $params->get('label') );
+$label = strtolower($label);
+
+if( strlen($label) == 0 ) $label = $videoid;
+
+JFactory::getDocument()->addScriptDeclaration('var tag=document.createElement(\'script\');'
+	.'tag.src="https://www.youtube.com/iframe_api";'
+	.'var firstScriptTag=document.getElementsByTagName(\'script\')[0];'
+	.'firstScriptTag.parentNode.insertBefore(tag,firstScriptTag);'
+	);
+
+$pretext	= $params->get('pretext');
+$posttext	= $params->get('posttext');
+
+require JModuleHelper::getLayoutPath('mod_youtube',$params->get('layout', 'default'));
---
+++ tmpl/default.php
@@ -0,0 +1,41 @@
+<?php defined('_JEXEC') or die;
+
+echo $pretext; ?>
+
+<div class="embed-responsive embed-responsive-16by9">
+	<div id="player-<?php echo $label; ?>" class="embed-responsive-item"></div>
+</div>
+
+<?php echo $posttext; ?>
+
+<script>
+var players = players || [];
+function onYouTubeIframeAPIReady() {
+	var player = new YT.Player('player-<?php echo $label; ?>', {
+	    height: '100%',
+	    width: '100%',
+<?php if($videoid): ?>
+		videoId: '<?php echo $videoid; ?>',
+<?php else: ?>
+	    playerVars: { listType:'playlist', list: '<?php echo $playlist; ?>' },
+<?php endif; ?>
+	    events: {
+	    	'onStateChange': function (event) {
+
+				if(typeof ga !== 'undefined') {
+					var label = '<?php echo $label; ?>';
+					switch (event.data) {
+						case YT.PlayerState.PLAYING:
+							ga('send', 'event', 'video', 'play', label); break;
+						case YT.PlayerState.PAUSED:
+							ga('send', 'event', 'video', 'pause', label); break;
+						case YT.PlayerState.ENDED:
+							ga('send', 'event', 'video', 'end', label); break;
+					}
+				}
+		    }
+		}
+	});
+	players.push(player);
+}
+</script>
